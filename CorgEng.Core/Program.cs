87c9c5902ece40bc3e710232693b593814a4a15d
PowerfulBacon <>
2022-03-23 22:24:08 +0000

    Update Program.cs

--- CorgEng.Core/Program.cs
+++ CorgEng.Core/Program.cs
@@ -22,10 +22,18 @@ namespace CorgEng.Core
     {
         static void Main(string[] args)
         {
+            //Initialize CorgEng
+            //This creates the window and loads all
+            //modules that are dependencies
             CorgEng.Initialize();
+            //Set the render core
             ExampleRenderCore erc = new ExampleRenderCore();
             CorgEng.SetRenderCore(erc);
+            //Transfer control of the main thread to the CorgEng
+            //rendering thread
             CorgEng.TransferToRenderingThread();
+            //Shut down the program once it has been closed
+            //and clean everything up.
             CorgEng.Shutdown();
         }
     }
