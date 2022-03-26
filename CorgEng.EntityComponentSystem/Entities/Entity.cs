f3236c4fafcc2cb16f9bef4d2ce629b03873ec3f
PowerfulBacon <>
2022-03-26 00:04:27 +0000

    Update Entity.cs

--- CorgEng.EntityComponentSystem/Entities/Entity.cs
+++ CorgEng.EntityComponentSystem/Entities/Entity.cs
@@ -1,4 +1,5 @@
 ﻿using CorgEng.EntityComponentSystem.Components;
+using CorgEng.EntityComponentSystem.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,43 +11,9 @@ namespace CorgEng.EntityComponentSystem.Entities
     public class Entity
     {
 
-        /// <summary>
-        /// A dictionary shared between all entities that records an entity and the component
-        /// instances it has.
-        /// Shared to save on memory.
-        /// </summary>
-        private static Dictionary<(Entity, Type), Component> EntityComponentInstances { get; } = new Dictionary<(Entity, Type), Component>();
+        List<Component> Components { get; } = new List<Component>();
 
-        /// <summary>
-        /// A list of the component types attatched to this entity
-        /// </summary>
-        private List<Type> ComponentTypes { get; } = new List<Type>();
-
-        /// <summary>
-        /// TODO: Populate this.
-        /// A dictionary that associates event types to a list of callbacks that this entity
-        /// needs to call should it recieve that event.
-        /// </summary>
-        public Dictionary<Type, Func<>>
-
-        /// <summary>
-        /// Get the component with the specified type
-        /// </summary>
-        public Component GetComponent<ComponentType>()
-        {
-            return EntityComponentInstances[(this, typeof(ComponentType))];
-        }
-
-        /// <summary>
-        /// Delete the entity, removes it's references from the static list.
-        /// </summary>
-        public void Delete()
-        {
-            foreach (Type type in ComponentTypes)
-            {
-                EntityComponentInstances.Remove((this, type));
-            }
-        }
+        Dictionary<Type, Func<Entity, Component, Event>> EventListeners;
 
     }
 }
