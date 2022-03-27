6ce501012e912007241db328bf66c073425e7422
PowerfulBacon <>
2022-03-27 12:19:25 +0100

    Updates logging

---
+++ CorgEng.Core/Logging/ILogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorgEng.Core.Logging
+{
+    public interface ILogger
+    {
+
+        void WriteLine(object message, LogType logType = LogType.MESSAGE);
+
+    }
+}
