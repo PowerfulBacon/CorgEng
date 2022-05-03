54bdfe905518ce72a4bbfa944716a7507b11b78b
PowerfulBacon <26465327+PowerfulBacon@users.noreply.github.com>
2022-05-03 18:20:26 +0100

    Updates the font test to test the last character as well as the first

--- CorgEng.Tests/FontTests/FontTests.cs
+++ CorgEng.Tests/FontTests/FontTests.cs
@@ -21,7 +21,7 @@ namespace CorgEng.Tests.FontTests
         public void TestLoadingSampleFont()
         {
             IFont defaultFont = FontFactory.GetFont("CourierCode");
-            //Test a few known characters
+            //Test a few known characters (First)
             IFontCharacter knownCharacter = defaultFont.GetCharacter(32);
             Assert.AreEqual(32, knownCharacter.CharacterCode, "Character code should be 32");
             Assert.AreEqual(2023, knownCharacter.TextureXPosition, "X code should be 2023");
@@ -31,6 +31,16 @@ namespace CorgEng.Tests.FontTests
             Assert.AreEqual(-2, knownCharacter.CharacterXOffset, "X Offset code should be -2");
             Assert.AreEqual(468, knownCharacter.CharacterYOffset, "Y Offset code should be 468");
             Assert.AreEqual(244, knownCharacter.CharacterXAdvance, "X Advance code should be 244");
+            //Test the last character
+            IFontCharacter lastCharacter = defaultFont.GetCharacter(126);
+            Assert.AreEqual(126, lastCharacter.CharacterCode, "Character code should be 126");
+            Assert.AreEqual(622, lastCharacter.TextureXPosition, "X code should be 622");
+            Assert.AreEqual(1563, lastCharacter.TextureYPosition, "Y code should be 1563");
+            Assert.AreEqual(191, lastCharacter.TextureWidth, "Width code should be 191");
+            Assert.AreEqual(46, lastCharacter.TextureHeight, "Height code should be 46");
+            Assert.AreEqual(27, lastCharacter.CharacterXOffset, "X Offset code should be 27");
+            Assert.AreEqual(201, lastCharacter.CharacterYOffset, "Y Offset code should be 201");
+            Assert.AreEqual(244, lastCharacter.CharacterXAdvance, "X Advance code should be 244");
         }
 
         [TestMethod]
