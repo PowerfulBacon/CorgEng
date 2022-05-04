677e3843696329f5d4a7d393d917ec65fa4cf51b
PowerfulBacon <26465327+PowerfulBacon@users.noreply.github.com>
2022-05-04 17:11:47 +0100

    Implements the start of text object

---
+++ CorgEng.GenericInterfaces/Rendering/Text/ITextObject.cs
@@ -0,0 +1,35 @@
+﻿using CorgEng.GenericInterfaces.UtilityTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorgEng.GenericInterfaces.Rendering.Text
+{
+    public interface ITextObject
+    {
+
+        /// <summary>
+        /// The text property that represents the text for this object.
+        /// Bindable, so that when changed, the renderable can automatically update.
+        /// </summary>
+        IBindableProperty<string> TextProperty { get; }
+
+        /// <summary>
+        /// The screen position of the text object.
+        /// </summary>
+        IBindableProperty<IVector<float>> ScreenPositionProperty { get; }
+
+        /// <summary>
+        /// Start rendering this text object
+        /// </summary>
+        void StartRendering();
+
+        /// <summary>
+        /// Stop rendering this text object
+        /// </summary>
+        void StopRendering();
+
+    }
+}
---
+++ CorgEng.Rendering/TextRendering/TextObject.cs
@@ -0,0 +1,40 @@
+﻿using CorgEng.Core.Dependencies;
+using CorgEng.GenericInterfaces.Rendering.Renderers.SpriteRendering;
+using CorgEng.GenericInterfaces.Rendering.Text;
+using CorgEng.GenericInterfaces.UtilityTypes;
+using CorgEng.UtilityTypes.BindableProperties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorgEng.Rendering.TextRendering
+{
+    internal class TextObject : ITextObject
+    {
+
+        public IBindableProperty<string> TextProperty { get; }
+
+        //Collect a sprite renderer dependency so that we can draw our glyphs.
+        [UsingDependency]
+        private static ISpriteRenderer SpriteRenderer;
+
+        public TextObject(string text)
+        {
+            //Create the text property
+            TextProperty = new BindableProperty<string>(text);
+        }
+
+        public void StartRendering()
+        {
+            
+        }
+
+        public void StopRendering()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
