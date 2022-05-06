e39c3b1ec5ded4dfef5caa839ae3be7a44422f8c
PowerfulBacon <>
2022-05-06 03:49:25 +0100

    Update TextObject.cs

--- CorgEng.Rendering/TextRendering/TextObject.cs
+++ CorgEng.Rendering/TextRendering/TextObject.cs
@@ -1,4 +1,6 @@
 ﻿using CorgEng.Core.Dependencies;
+using CorgEng.GenericInterfaces.Font.Characters;
+using CorgEng.GenericInterfaces.Font.Fonts;
 using CorgEng.GenericInterfaces.Rendering.Renderers.SpriteRendering;
 using CorgEng.GenericInterfaces.Rendering.Text;
 using CorgEng.GenericInterfaces.UtilityTypes;
@@ -14,10 +16,17 @@ namespace CorgEng.Rendering.TextRendering
     internal class TextObject : ITextObject
     {
 
+        private IFont font;
+
         public IBindableProperty<string> TextProperty { get; }
 
-        //TODO
         public IBindableProperty<IVector<float>> ScreenPositionProperty { get; }
+
+        public IBindableProperty<float> WidthProperty { get; }
+
+        public IBindableProperty<float> HeightProperty { get; }
+
+        public IBindableProperty<float> Scale { get; }
 
         //Collect a sprite renderer dependency so that we can draw our glyphs.
         [UsingDependency]
@@ -29,9 +38,27 @@ namespace CorgEng.Rendering.TextRendering
             TextProperty = new BindableProperty<string>(text);
         }
 
+        /// <summary>
+        /// Start rendering this text object.
+        /// </summary>
         public void StartRendering()
         {
-            
+            double xPointer = ScreenPositionProperty.Value.X;
+            double yPointer = ScreenPositionProperty.Value.Y;
+            foreach (char character in TextProperty.Value)
+            {
+                //Get the character
+                IFontCharacter fontCharacter = font.GetCharacter(character);
+                //Render the character
+                //Increment the pointers
+                xPointer += fontCharacter.CharacterXAdvance * Scale.Value;
+                //Moved past specified width, wrap around
+                if (xPointer >= ScreenPositionProperty.Value.X + WidthProperty.Value)
+                {
+                    xPointer = ScreenPositionProperty.Value.X;
+                    yPointer += Scale.Value;
+                }
+            }
         }
 
         public void StopRendering()
