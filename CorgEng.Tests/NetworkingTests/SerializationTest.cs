82e923f168a7ab2c5e012359b18cf819f36f9c45
PowerfulBacon <26465327+PowerfulBacon@users.noreply.github.com>
2022-05-11 16:50:15 +0100

    fasdf

--- CorgEng.Tests/NetworkingTests/SerializationTest.cs
+++ CorgEng.Tests/NetworkingTests/SerializationTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,15 @@ using System.Threading.Tasks;
 
 namespace CorgEng.Tests.NetworkingTests
 {
-    class SerializationTest
+    [TestClass]
+    public class SerializationTest
     {
+
+        [TestMethod]
+        public void TestSerialization()
+        {
+            //Get all event classes
+        }
+
     }
 }
