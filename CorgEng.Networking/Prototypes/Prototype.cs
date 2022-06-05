6f076e369365aaf1a381f485d50e00ff9497db48
PowerfulBacon <26465327+PowerfulBacon@users.noreply.github.com>
2022-06-05 21:34:57 +0100

    Update Prototype.cs

--- CorgEng.Networking/Prototypes/Prototype.cs
+++ CorgEng.Networking/Prototypes/Prototype.cs
@@ -1,8 +1,11 @@
-﻿using CorgEng.GenericInterfaces.EntityComponentSystem;
+﻿using CorgEng.EntityComponentSystem.Entities;
+using CorgEng.GenericInterfaces.EntityComponentSystem;
 using CorgEng.GenericInterfaces.Networking.PrototypeManager;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,12 +18,30 @@ namespace CorgEng.Networking.Prototypes
 
         public uint Identifier { get; set; } = PrototypeIdentifierHighest++;
 
+        Dictionary<Type, Dictionary<PropertyInfo, object>> prototypeComponents = new Dictionary<Type, Dictionary<PropertyInfo, object>>();
+
         public IEntity CreateEntityFromPrototype()
         {
+            IEntity createdEntity = new Entity();
+            //Add components
+            foreach (Type type in prototypeComponents.Keys)
+            {
+                //Create the uninitialized component
+                IComponent createdComponent = (IComponent)FormatterServices.GetUninitializedObject(type);
+                //Inject variables
+                foreach (PropertyInfo propertyInfo in prototypeComponents[type].Keys)
+                {
+                    propertyInfo.SetValue(createdComponent, prototypeComponents[type][propertyInfo]);
+                }
+                //Add the component
+                createdEntity.AddComponent(createdComponent);
+            }
+            return createdEntity;
         }
 
         public void DeserializePrototype(byte[] data)
         {
+            null;
         }
 
         public byte[] SerializePrototype()
