52320c614f49a8053b3d32e4978db15877522b11
PowerfulBacon <26465327+PowerfulBacon@users.noreply.github.com>
2022-06-22 19:46:39 +0100

    Adds in tests for entity to prototype speed and prototype to entity speed

--- CorgEng.Tests/Performance/SerialisationPerformance.cs
+++ CorgEng.Tests/Performance/SerialisationPerformance.cs
@@ -29,6 +29,68 @@ namespace CorgEng.Tests.Performance
 #endif
 
         [TestMethod]
+        public void TestEntityToPrototypePerformance()
+        {
+            //Create an entity
+            IEntity entity = new Entity();
+            TestComponent testComponent = new TestComponent();
+            testComponent.Integer = 59;
+            testComponent.Text = "Hello World!";
+            testComponent.Double = 3.14159265;
+            entity.AddComponent(testComponent);
+
+            //Perform the tests
+            bool running = true;
+            int runs = 0;
+            Thread thread = new Thread(() => {
+                while (running)
+                {
+                    //Perform a run
+                    PrototypeManager.GetPrototype(entity, false);
+                    //Run completed
+                    runs++;
+                }
+            });
+            thread.Start();
+            Thread.Sleep(TEST_TIME);
+            running = false;
+            //Process results
+            Assert.Inconclusive($"Performed {runs} prototype gets in {TEST_TIME}ms at a rate of {runs / (TEST_TIME / 1000f)}/s");
+        }
+
+        [TestMethod]
+        public void TestPrototypeToEntityPerformance()
+        {
+            //Create an entity
+            IEntity entity = new Entity();
+            TestComponent testComponent = new TestComponent();
+            testComponent.Integer = 59;
+            testComponent.Text = "Hello World!";
+            testComponent.Double = 3.14159265;
+            entity.AddComponent(testComponent);
+            //Serialize the entity's prototype
+            IPrototype collectedPrototype = PrototypeManager.GetPrototype(entity, false);
+
+            //Perform the tests
+            bool running = true;
+            int runs = 0;
+            Thread thread = new Thread(() => {
+                while (running)
+                {
+                    //Perform a run
+                    collectedPrototype.CreateEntityFromPrototype();
+                    //Run completed
+                    runs++;
+                }
+            });
+            thread.Start();
+            Thread.Sleep(TEST_TIME);
+            running = false;
+            //Process results
+            Assert.Inconclusive($"Performed {runs} instantiations in {TEST_TIME}ms at a rate of {runs / (TEST_TIME / 1000f)}/s");
+        }
+
+        [TestMethod]
         public void TestDeserialisationPerformance()
         {
 
