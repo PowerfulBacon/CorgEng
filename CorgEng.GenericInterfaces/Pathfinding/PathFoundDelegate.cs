66e99c58cfa830515e781e0bb56db45b8c574ccd
PowerfulBacon <26465327+PowerfulBacon@users.noreply.github.com>
2022-08-16 20:54:13 +0100

    Pathfinding interface

---
+++ CorgEng.GenericInterfaces/Pathfinding/PathFoundDelegate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorgEng.GenericInterfaces.Pathfinding
+{
+    internal class PathFoundDelegate
+    {
+    }
+}
