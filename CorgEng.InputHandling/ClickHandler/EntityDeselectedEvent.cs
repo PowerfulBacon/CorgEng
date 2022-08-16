63f4cd2298be1eaa7a9b77e36fb9c5bc04810f2c
PowerfulBacon <26465327+PowerfulBacon@users.noreply.github.com>
2022-08-16 10:28:34 +0100

    Some minor changes and fixes to the world click handler

---
+++ CorgEng.InputHandling/ClickHandler/EntityDeselectedEvent.cs
@@ -0,0 +1,27 @@
+﻿using CorgEng.GenericInterfaces.EntityComponentSystem;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorgEng.InputHandling.ClickHandler
+{
+    public class EntityDeselectedEvent : INetworkedEvent
+    {
+        public bool CanBeRaisedByClient => true;
+
+        public void Deserialise(BinaryReader reader)
+        { }
+
+        public void Serialise(BinaryWriter writer)
+        { }
+
+        public int SerialisedLength()
+        {
+            return 0;
+        }
+
+    }
+}
