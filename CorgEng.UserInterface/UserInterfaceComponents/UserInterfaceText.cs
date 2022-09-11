2e801141d38cf838e8e3c184c55352816f9c5ef1
PowerfulBacon <26465327+PowerfulBacon@users.noreply.github.com>
2022-09-11 22:08:53 +0100

    Create UserInterfaceText.cs

---
+++ CorgEng.UserInterface/UserInterfaceComponents/UserInterfaceText.cs
@@ -0,0 +1,33 @@
+﻿using CorgEng.Core.Dependencies;
+using CorgEng.GenericInterfaces.Font.Fonts;
+using CorgEng.GenericInterfaces.Rendering.Text;
+using CorgEng.GenericInterfaces.UserInterface.Anchors;
+using CorgEng.GenericInterfaces.UserInterface.Components;
+using CorgEng.UserInterface.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorgEng.UserInterface.UserInterfaceComponents
+{
+    internal class UserInterfaceText : UserInterfaceComponent
+    {
+
+        [UsingDependency]
+        private static IFontFactory FontFactory;
+
+        [UsingDependency]
+        private static ITextObjectFactory TextObjectFactory;
+
+        public UserInterfaceText(IAnchor anchorDetails, IDictionary<string, string> arguments) : base(anchorDetails, arguments)
+        { }
+
+        public UserInterfaceText(IUserInterfaceComponent parent, IAnchor anchorDetails, IDictionary<string, string> arguments) : base(parent, anchorDetails, arguments)
+        { }
+
+
+
+    }
+}
