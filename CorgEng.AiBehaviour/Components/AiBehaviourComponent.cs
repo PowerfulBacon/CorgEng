cd5fbd1347ce485d7a5c85971e58ca52eff1c10e
PowerfulBacon <26465327+PowerfulBacon@users.noreply.github.com>
2022-09-21 19:55:37 +0100

    Brings AI behaviour and action processing up to CorgEng

---
+++ CorgEng.AiBehaviour/Components/AiBehaviourComponent.cs
@@ -0,0 +1,28 @@
+﻿using CorgEng.EntityComponentSystem.Components;
+using CorgEng.GenericInterfaces.AiBehaviours;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorgEng.AiBehaviour.Components
+{
+    /// <summary>
+    /// Class that holds the behaviour controller
+    /// for pawns.
+    /// This is basically the brain of the pawn,
+    /// it decides what to do and then other components
+    /// will handle it.
+    /// </summary>
+    public class AiBehaviourComponent : Component
+    {
+
+        /// <summary>
+        /// The behaviour manager for this entity.
+        /// </summary>
+        public IBehaviourManager BehaviourManager { get; set; }
+
+    }
+
+}
