a7993c9264b684e84f350aa5ec5fa4eff05c8d60
PowerfulBacon <26465327+PowerfulBacon@users.noreply.github.com>
2022-10-22 12:04:41 +0100

    Fixes multiple batches breaking on update

--- CorgEng.Tests/RenderingTests/BindedBatchTest.cs
+++ CorgEng.Tests/RenderingTests/BindedBatchTest.cs
@@ -207,5 +207,188 @@ namespace CorgEng.Tests.RenderingTests
 
         }
 
+        [TestMethod]
+        public void TestOverflowBatches()
+        {
+            //Create first property and add it
+            IBindableProperty<IVector<float>> property1 = new BindableProperty<IVector<float>>(new Vector<float>(1, 1, 1));
+            IBindableProperty<IVector<float>> property2 = new BindableProperty<IVector<float>>(new Vector<float>(1, 1));
+            IBindableProperty<IVector<float>> property3 = new BindableProperty<IVector<float>>(new Vector<float>(1));
+
+            TestBatch batch = new TestBatch(1);
+
+            IBatchElement<TestBatch> batchElement = new BatchElement<TestBatch>(
+                new IBindableProperty<IVector<float>>[] {
+                    property1,
+                    property2,
+                    property3,
+                });
+            batch.Add(batchElement);
+
+            //Test adding
+
+            Assert.AreEqual(0, batchElement.BatchPosition, "Expected 1st element in place 0.");
+            Assert.AreEqual(1, batch.GetArray(batchElement.BatchPosition / batch.BatchSize, 0)[(batchElement.BatchPosition % batch.BatchSize) + 0], "Batch adding failure");
+            Assert.AreEqual(1, batch.GetArray(batchElement.BatchPosition / batch.BatchSize, 0)[(batchElement.BatchPosition % batch.BatchSize) + 1], "Batch adding failure");
+            Assert.AreEqual(1, batch.GetArray(batchElement.BatchPosition / batch.BatchSize, 0)[(batchElement.BatchPosition % batch.BatchSize) + 2], "Batch adding failure");
+            Assert.AreEqual(1, batch.GetArray(batchElement.BatchPosition / batch.BatchSize, 1)[(batchElement.BatchPosition % batch.BatchSize) + 0], "Batch adding failure");
+            Assert.AreEqual(1, batch.GetArray(batchElement.BatchPosition / batch.BatchSize, 1)[(batchElement.BatchPosition % batch.BatchSize) + 1], "Batch adding failure");
+            Assert.AreEqual(1, batch.GetArray(batchElement.BatchPosition / batch.BatchSize, 2)[(batchElement.BatchPosition % batch.BatchSize) + 0], "Batch adding failure");
+
+            //Test updating
+
+            property1.Value.X = 2;
+            property1.Value.Y = 2;
+            property1.Value.Z = 2;
+            property1.TriggerChanged();
+            property2.Value.X = 2;
+            property2.Value.Y = 2;
+            property2.TriggerChanged();
+            property3.Value.X = 2;
+            property3.TriggerChanged();
+
+            Assert.AreEqual(2, batch.GetArray(batchElement.BatchPosition / batch.BatchSize, 0)[3 * (batchElement.BatchPosition % batch.BatchSize) + 0], "Batch updating failure");
+            Assert.AreEqual(2, batch.GetArray(batchElement.BatchPosition / batch.BatchSize, 0)[3 * (batchElement.BatchPosition % batch.BatchSize) + 1], "Batch updating failure");
+            Assert.AreEqual(2, batch.GetArray(batchElement.BatchPosition / batch.BatchSize, 0)[3 * (batchElement.BatchPosition % batch.BatchSize) + 2], "Batch updating failure");
+            Assert.AreEqual(2, batch.GetArray(batchElement.BatchPosition / batch.BatchSize, 1)[2 * (batchElement.BatchPosition % batch.BatchSize) + 0], "Batch updating failure");
+            Assert.AreEqual(2, batch.GetArray(batchElement.BatchPosition / batch.BatchSize, 1)[2 * (batchElement.BatchPosition % batch.BatchSize) + 1], "Batch updating failure");
+            Assert.AreEqual(2, batch.GetArray(batchElement.BatchPosition / batch.BatchSize, 2)[1 * (batchElement.BatchPosition % batch.BatchSize) + 0], "Batch updating failure");
+
+            //Add another element
+
+            IBindableProperty<IVector<float>> property12 = new BindableProperty<IVector<float>>(new Vector<float>(3, 3, 3));
+            IBindableProperty<IVector<float>> property22 = new BindableProperty<IVector<float>>(new Vector<float>(3, 3));
+            IBindableProperty<IVector<float>> property32 = new BindableProperty<IVector<float>>(new Vector<float>(3));
+            IBatchElement<TestBatch> batchElement2 = new BatchElement<TestBatch>(
+                new IBindableProperty<IVector<float>>[] {
+                    property12,
+                    property22,
+                    property32,
+                });
+            batch.Add(batchElement2);
+
+            //Test adding
+
+            Assert.AreEqual(1, batchElement2.BatchPosition, "Expected 2nd element in place 1.");
+            Assert.AreEqual(3, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 0)[3 * (batchElement2.BatchPosition % batch.BatchSize) + 0], "Batch second add failure");
+            Assert.AreEqual(3, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 0)[3 * (batchElement2.BatchPosition % batch.BatchSize) + 1], "Batch second add failure");
+            Assert.AreEqual(3, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 0)[3 * (batchElement2.BatchPosition % batch.BatchSize) + 2], "Batch second add failure");
+            Assert.AreEqual(3, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 1)[2 * (batchElement2.BatchPosition % batch.BatchSize) + 0], "Batch second add failure");
+            Assert.AreEqual(3, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 1)[2 * (batchElement2.BatchPosition % batch.BatchSize) + 1], "Batch second add failure");
+            Assert.AreEqual(3, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 2)[1 * (batchElement2.BatchPosition % batch.BatchSize) + 0], "Batch second add failure");
+
+            //Test updating
+
+            property12.Value.X = 4;
+            property12.Value.Y = 4;
+            property12.Value.Z = 4;
+            property12.TriggerChanged();
+            property22.Value.X = 4;
+            property22.Value.Y = 4;
+            property22.TriggerChanged();
+            property32.Value.X = 4;
+            property32.TriggerChanged();
+
+            Assert.AreEqual(4, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 0)[3 * (batchElement2.BatchPosition % batch.BatchSize) + 0], "Batch second update failure");
+            Assert.AreEqual(4, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 0)[3 * (batchElement2.BatchPosition % batch.BatchSize) + 1], "Batch second update failure");
+            Assert.AreEqual(4, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 0)[3 * (batchElement2.BatchPosition % batch.BatchSize) + 2], "Batch second update failure");
+            Assert.AreEqual(4, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 1)[2 * (batchElement2.BatchPosition % batch.BatchSize) + 0], "Batch second update failure");
+            Assert.AreEqual(4, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 1)[2 * (batchElement2.BatchPosition % batch.BatchSize) + 1], "Batch second update failure");
+            Assert.AreEqual(4, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 2)[1 * (batchElement2.BatchPosition % batch.BatchSize) + 0], "Batch second update failure");
+
+            //Remove
+            batch.Remove(batchElement);
+
+            //Test
+            Assert.AreEqual(4, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 0)[3 * (batchElement2.BatchPosition % batch.BatchSize) + 0], "Batch removal failure");
+            Assert.AreEqual(4, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 0)[3 * (batchElement2.BatchPosition % batch.BatchSize) + 1], "Batch removal failure");
+            Assert.AreEqual(4, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 0)[3 * (batchElement2.BatchPosition % batch.BatchSize) + 2], "Batch removal failure");
+            Assert.AreEqual(4, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 1)[2 * (batchElement2.BatchPosition % batch.BatchSize) + 0], "Batch removal failure");
+            Assert.AreEqual(4, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 1)[2 * (batchElement2.BatchPosition % batch.BatchSize) + 1], "Batch removal failure");
+            Assert.AreEqual(4, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 2)[1 * (batchElement2.BatchPosition % batch.BatchSize) + 0], "Batch removal failure");
+
+            //Test changing
+
+            property12.Value.X = 5;
+            property12.Value.Y = 5;
+            property12.Value.Z = 5;
+            property12.TriggerChanged();
+            property22.Value.X = 5;
+            property22.Value.Y = 5;
+            property22.TriggerChanged();
+            property32.Value.X = 5;
+            property32.TriggerChanged();
+
+            Assert.AreEqual(5, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 0)[3 * (batchElement2.BatchPosition % batch.BatchSize) + 0], "Batch update post-removal failure");
+            Assert.AreEqual(5, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 0)[3 * (batchElement2.BatchPosition % batch.BatchSize) + 1], "Batch update post-removal failure");
+            Assert.AreEqual(5, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 0)[3 * (batchElement2.BatchPosition % batch.BatchSize) + 2], "Batch update post-removal failure");
+            Assert.AreEqual(5, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 1)[2 * (batchElement2.BatchPosition % batch.BatchSize) + 0], "Batch update post-removal failure");
+            Assert.AreEqual(5, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 1)[2 * (batchElement2.BatchPosition % batch.BatchSize) + 1], "Batch update post-removal failure");
+            Assert.AreEqual(5, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 2)[1 * (batchElement2.BatchPosition % batch.BatchSize) + 0], "Batch update post-removal failure");
+
+            batch.Add(batchElement);
+
+            //Test adding
+
+            Assert.AreEqual(2, batch.GetArray(batchElement.BatchPosition / batch.BatchSize, 0)[3 * (batchElement.BatchPosition % batch.BatchSize) + 0], "Adding A, Adding B, Removing A, Adding A results in an incorrect batch configuration.");
+            Assert.AreEqual(2, batch.GetArray(batchElement.BatchPosition / batch.BatchSize, 0)[3 * (batchElement.BatchPosition % batch.BatchSize) + 1], "Adding A, Adding B, Removing A, Adding A results in an incorrect batch configuration.");
+            Assert.AreEqual(2, batch.GetArray(batchElement.BatchPosition / batch.BatchSize, 0)[3 * (batchElement.BatchPosition % batch.BatchSize) + 2], "Adding A, Adding B, Removing A, Adding A results in an incorrect batch configuration.");
+            Assert.AreEqual(2, batch.GetArray(batchElement.BatchPosition / batch.BatchSize, 1)[2 * (batchElement.BatchPosition % batch.BatchSize) + 0], "Adding A, Adding B, Removing A, Adding A results in an incorrect batch configuration.");
+            Assert.AreEqual(2, batch.GetArray(batchElement.BatchPosition / batch.BatchSize, 1)[2 * (batchElement.BatchPosition % batch.BatchSize) + 1], "Adding A, Adding B, Removing A, Adding A results in an incorrect batch configuration.");
+            Assert.AreEqual(2, batch.GetArray(batchElement.BatchPosition / batch.BatchSize, 2)[1 * (batchElement.BatchPosition % batch.BatchSize) + 0], "Adding A, Adding B, Removing A, Adding A results in an incorrect batch configuration.");
+
+            //Test updating
+
+            property1.Value.X = 6;
+            property1.Value.Y = 6;
+            property1.Value.Z = 6;
+            property1.TriggerChanged();
+            property2.Value.X = 6;
+            property2.Value.Y = 6;
+            property2.TriggerChanged();
+            property3.Value.X = 6;
+            property3.TriggerChanged();
+
+            Assert.AreEqual(6, batch.GetArray(batchElement.BatchPosition / batch.BatchSize, 0)[3 * (batchElement.BatchPosition % batch.BatchSize) + 0], "Adding A, Adding B, Removing A, Adding A then updating A results in an incorrect batch configuration");
+            Assert.AreEqual(6, batch.GetArray(batchElement.BatchPosition / batch.BatchSize, 0)[3 * (batchElement.BatchPosition % batch.BatchSize) + 1], "Adding A, Adding B, Removing A, Adding A then updating A results in an incorrect batch configuration");
+            Assert.AreEqual(6, batch.GetArray(batchElement.BatchPosition / batch.BatchSize, 0)[3 * (batchElement.BatchPosition % batch.BatchSize) + 2], "Adding A, Adding B, Removing A, Adding A then updating A results in an incorrect batch configuration");
+            Assert.AreEqual(6, batch.GetArray(batchElement.BatchPosition / batch.BatchSize, 1)[2 * (batchElement.BatchPosition % batch.BatchSize) + 0], "Adding A, Adding B, Removing A, Adding A then updating A results in an incorrect batch configuration");
+            Assert.AreEqual(6, batch.GetArray(batchElement.BatchPosition / batch.BatchSize, 1)[2 * (batchElement.BatchPosition % batch.BatchSize) + 1], "Adding A, Adding B, Removing A, Adding A then updating A results in an incorrect batch configuration");
+            Assert.AreEqual(6, batch.GetArray(batchElement.BatchPosition / batch.BatchSize, 2)[1 * (batchElement.BatchPosition % batch.BatchSize) + 0], "Adding A, Adding B, Removing A, Adding A then updating A results in an incorrect batch configuration");
+            Assert.AreEqual(5, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 0)[3 * (batchElement2.BatchPosition % batch.BatchSize) + 0], "(UNEXPECTED SIDE EFFECTS) Adding A, Adding B, Removing A, Adding A then updating A results in an incorrect batch configuration");
+            Assert.AreEqual(5, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 0)[3 * (batchElement2.BatchPosition % batch.BatchSize) + 1], "(UNEXPECTED SIDE EFFECTS) Adding A, Adding B, Removing A, Adding A then updating A results in an incorrect batch configuration");
+            Assert.AreEqual(5, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 0)[3 * (batchElement2.BatchPosition % batch.BatchSize) + 2], "(UNEXPECTED SIDE EFFECTS) Adding A, Adding B, Removing A, Adding A then updating A results in an incorrect batch configuration");
+            Assert.AreEqual(5, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 1)[2 * (batchElement2.BatchPosition % batch.BatchSize) + 0], "(UNEXPECTED SIDE EFFECTS) Adding A, Adding B, Removing A, Adding A then updating A results in an incorrect batch configuration");
+            Assert.AreEqual(5, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 1)[2 * (batchElement2.BatchPosition % batch.BatchSize) + 1], "(UNEXPECTED SIDE EFFECTS) Adding A, Adding B, Removing A, Adding A then updating A results in an incorrect batch configuration");
+            Assert.AreEqual(5, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 2)[1 * (batchElement2.BatchPosition % batch.BatchSize) + 0], "(UNEXPECTED SIDE EFFECTS) Adding A, Adding B, Removing A, Adding A then updating A results in an incorrect batch configuration");
+
+            //Test changing
+
+            property12.Value.X = 7;
+            property12.Value.Y = 7;
+            property12.Value.Z = 7;
+            property12.TriggerChanged();
+            property22.Value.X = 7;
+            property22.Value.Y = 7;
+            property22.TriggerChanged();
+            property32.Value.X = 7;
+            property32.TriggerChanged();
+
+            Assert.AreEqual(7, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 0)[3 * (batchElement2.BatchPosition % batch.BatchSize) + 0], "Adding A, Adding B, Removing A, Adding A then updating B results in an incorrect batch configuration");
+            Assert.AreEqual(7, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 0)[3 * (batchElement2.BatchPosition % batch.BatchSize) + 1], "Adding A, Adding B, Removing A, Adding A then updating B results in an incorrect batch configuration");
+            Assert.AreEqual(7, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 0)[3 * (batchElement2.BatchPosition % batch.BatchSize) + 2], "Adding A, Adding B, Removing A, Adding A then updating B results in an incorrect batch configuration");
+            Assert.AreEqual(7, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 1)[2 * (batchElement2.BatchPosition % batch.BatchSize) + 0], "Adding A, Adding B, Removing A, Adding A then updating B results in an incorrect batch configuration");
+            Assert.AreEqual(7, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 1)[2 * (batchElement2.BatchPosition % batch.BatchSize) + 1], "Adding A, Adding B, Removing A, Adding A then updating B results in an incorrect batch configuration");
+            Assert.AreEqual(7, batch.GetArray(batchElement2.BatchPosition / batch.BatchSize, 2)[1 * (batchElement2.BatchPosition % batch.BatchSize) + 0], "Adding A, Adding B, Removing A, Adding A then updating B results in an incorrect batch configuration");
+            Assert.AreEqual(6, batch.GetArray(batchElement.BatchPosition / batch.BatchSize, 0)[3 * (batchElement.BatchPosition % batch.BatchSize) + 0], "(UNEXPECTED SIDE EFFECTS) Adding A, Adding B, Removing A, Adding A then updating B results in an incorrect batch configuration");
+            Assert.AreEqual(6, batch.GetArray(batchElement.BatchPosition / batch.BatchSize, 0)[3 * (batchElement.BatchPosition % batch.BatchSize) + 1], "(UNEXPECTED SIDE EFFECTS) Adding A, Adding B, Removing A, Adding A then updating B results in an incorrect batch configuration");
+            Assert.AreEqual(6, batch.GetArray(batchElement.BatchPosition / batch.BatchSize, 0)[3 * (batchElement.BatchPosition % batch.BatchSize) + 2], "(UNEXPECTED SIDE EFFECTS) Adding A, Adding B, Removing A, Adding A then updating B results in an incorrect batch configuration");
+            Assert.AreEqual(6, batch.GetArray(batchElement.BatchPosition / batch.BatchSize, 1)[2 * (batchElement.BatchPosition % batch.BatchSize) + 0], "(UNEXPECTED SIDE EFFECTS) Adding A, Adding B, Removing A, Adding A then updating B results in an incorrect batch configuration");
+            Assert.AreEqual(6, batch.GetArray(batchElement.BatchPosition / batch.BatchSize, 1)[2 * (batchElement.BatchPosition % batch.BatchSize) + 1], "(UNEXPECTED SIDE EFFECTS) Adding A, Adding B, Removing A, Adding A then updating B results in an incorrect batch configuration");
+            Assert.AreEqual(6, batch.GetArray(batchElement.BatchPosition / batch.BatchSize, 2)[1 * (batchElement.BatchPosition % batch.BatchSize) + 0], "(UNEXPECTED SIDE EFFECTS) Adding A, Adding B, Removing A, Adding A then updating B results in an incorrect batch configuration");
+
+
+        }
+
     }
 }
