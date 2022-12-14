bc94886441a189a1d177c772b9d585eaa4e76c24
PowerfulBacon <26465327+PowerfulBacon@users.noreply.github.com>
2022-12-14 15:17:03 +0000

    Adds in more stuff to the example project (#143)

---
+++ CorgEng.Example.Shared/Components/SandFactory/SandFactoryComponent.cs
@@ -0,0 +1,13 @@
+﻿using CorgEng.EntityComponentSystem.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorgEng.Example.Shared.Components.SandFactory
+{
+    public class SandFactoryComponent : Component
+    {
+    }
+}
