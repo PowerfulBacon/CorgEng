15fd00aec1c1593fe81396cfdb9ab1cb8ec00cb4
PowerfulBacon <26465327+PowerfulBacon@users.noreply.github.com>
2023-02-02 13:56:25 +0000

    Stuff

---
+++ CorgEng.Physics/PhysicsBodies/PhysicsBody.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorgEng.Physics.PhysicsBodies
+{
+    internal class PhysicsBody
+    {
+
+        public PhysicsTransform PhysicsTransform { get; set; }
+
+    }
+}
